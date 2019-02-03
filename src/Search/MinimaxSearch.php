37fa45ff2494dbb7d36a9d5e6267106663cfeb00
Arkadiusz Kondas <arkadiusz.kondas@gmail.com>
2019-02-03 09:02:57 +0100

    Update phpunit to 8.0

--- src/Search/MinimaxSearch.php
+++ src/Search/MinimaxSearch.php
@@ -34,10 +34,6 @@ final class MinimaxSearch implements Search
         $bestValue = $whiteTurn ? 99999 : -99999;
         $bestMove = null;
 
-        if (count($moves) === 0) {
-            return null;
-        }
-
         foreach ($moves as $san) {
             $board->move($san);
             $newValue = $this->minimax($this->depth - 1, -100000, 100000, $board);
@@ -53,13 +49,11 @@ final class MinimaxSearch implements Search
 
     private function minimax(int $depth, int $alpha, int $beta, Chessboard $board): int
     {
-        $whiteTurn = $board->isWhiteTurn();
-
         if ($depth === 0) {
             return -$this->evaluator->evaluate($board);
         }
 
-        if ($whiteTurn) {
+        if ($board->isWhiteTurn()) {
             $bestValue = 99999;
             foreach ($board->moves() as $san) {
                 $board->move($san);
